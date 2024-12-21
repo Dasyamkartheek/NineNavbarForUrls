1bc3f06f43377f1730a16e995e227fa15b16515f
DasyamKartheek <satyadasyam8@gmail.com>
2024-12-21 15:26:52 +0530

    kar

--- NineNavbarForUrls/settings.py
+++ NineNavbarForUrls/settings.py
@@ -124,7 +124,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[BASE_DIR/'Static'],
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage",
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
